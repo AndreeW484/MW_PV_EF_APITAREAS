4182bfe841bef0929d6bb39c124f9690e73b0414
AndreeW484 <mynorw8@gmail.com>
2024-06-22 16:18:35 -0600

    Rutas elaboradas para cada Endpoint

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://AndreeW484:Invierno2024@127.0.0.1:3306/apiflask'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-#Instanciación de la base de datos y del serializador
+# Instanciación de la base de datos y del serializador
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
@@ -22,8 +22,61 @@ class Tarea(db.Model):
 # Creación de la clase Schema para aplicar apropiadamente el modelo y procesar bien cada registro de entrada como de salida.
 class TareaSchema(SQLAlchemyAutoSchema):
     class Meta:
-        modelo = Tarea
+        model = Tarea
         load_instance = True
 
 tarea_schema = TareaSchema()
-tarea_schema = TareaSchema(many=True)
+tareas_schema = TareaSchema(many=True)
+
+# Elaboración de rutas API's/Endpoints empleando Flask
+@app.route('/tarea', methods=['POST'])
+def agregar_tarea():
+    descripcion_tarea = request.json['descripcion_tarea']
+    fecha_maxima_realizacion = request.json['fecha_maxima_realizacion']
+
+    nueva_tarea = Tarea(descripcion_tarea=descripcion_tarea, fecha_maxima_realizacion=fecha_maxima_realizacion)
+    db.session.add(nueva_tarea)
+    db.session.commit()
+    return jsonify(tarea_schema.dump(nueva_tarea)), 201
+
+@app.route('/tarea', methods=['GET'])
+def obtener_tareas():
+    tareas = Tarea.query.all()
+    resultado = tareas_schema.dump(tareas)
+    return jsonify(resultado)
+
+@app.route('/tarea/<int:id>', methods=['GET'])
+def obtener_tarea(id):
+    tarea = Tarea.query.get(id)
+    if tarea is None:
+        return jsonify({'message': 'Tarea no encontrada por el id proporcionado'}), 404
+    return jsonify(tarea_schema.dump(tarea))
+
+@app.route('/tarea/<int:id>', methods=['DELETE'])
+def borrar_tarea(id):
+    tarea = Tarea.query.get(id)
+    if tarea is None:
+        return jsonify({'message': 'Tarea no encontrada por el id proporcionado'}), 404
+    db.session.delete(tarea)
+    db.session.commit()
+    return jsonify({'message': 'Tarea eliminada exitosamente'})
+
+@app.route('/tarea/<int:id>', methods=['PUT'])
+def actualizar_tarea(id):
+    tarea = Tarea.query.get(id)
+    if tarea is None:
+        return jsonify({'message': 'Tarea no encontrada por el id proporcionado'}), 404
+
+    descripcion_tarea = request.json['descripcion_tarea']
+    fecha_maxima_realizacion = request.json['fecha_maxima_realizacion']
+
+    tarea.descripcion_tarea = descripcion_tarea
+    tarea.fecha_maxima_realizacion = fecha_maxima_realizacion
+
+    db.session.commit()
+    return jsonify(tarea_schema.dump(tarea))
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
